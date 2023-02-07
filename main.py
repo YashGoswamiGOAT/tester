8435989c5c72f22df5c7283887993221a9f8a2bb
YashGoswamiGOAT <124524109+YashGoswamiGOAT@users.noreply.github.com>
2023-02-08 01:30:41 +0530

    Update main.py

--- main.py
+++ main.py
@@ -4,11 +4,12 @@ app = Flask(__name__)
 
 @app.route("/")
 def ind():
+    open("yash.txt","w").write("YASH ROCKS")
     return "Hello"
 
 @app.route("/yash")
 def red():
-    return str(requests.get("https://yashgoswamigoat.pythonanywhere.com/webservice/notifications").text)
+    return str(requests.get("https://yashgoswamigoat.pythonanywhere.com/webservice/notifications").text)+str(open("yash.txt","r").read())
 
 if __name__=="__main__":
     app.run()
